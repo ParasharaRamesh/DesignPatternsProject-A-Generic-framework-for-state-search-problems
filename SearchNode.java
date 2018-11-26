0a10b690b3943eed4d0a8a377fdd0d23d9a4421b
Parashara Ramesh <parasharar@gmail.com>
2018-11-26 23:43:17 +0530

    have to do controller and searchPath + decorater

--- SearchNode.java
+++ SearchNode.java
@@ -1,12 +1,16 @@
 public class SearchNode<State> {
     // member variables
     private State state;
+    private String transformation;
 
     // constructor
     public SearchNode(State state) {
         this.state = state;
     }
 
+    public setTransformation(String transformation){
+        this.transformation = transformation;
+    }
     // transformation functions applicable on each searchnode
 
     // fill later
--- State.java
+++ State.java
@@ -1,11 +1,21 @@
 
 //domain dependent
 class JugState {
+    // capacities of each jug
+    private static Integer capacityJug1 = 5;
+    private static Integer capacityJug2 = 7;
+
+    // // static init block
+    // static {
+    // capacityJug1 = 5;
+    // capacityJug2 = 7;
+    // }
+
     // member variables
     private Integer jug1;
     private Integer jug2;
 
-    public JugState(int jug1, int jug2) {
+    public JugState(Integer jug1, Integer jug2) {
         this.jug1 = jug1;
         this.jug2 = jug2;
     }
@@ -16,15 +26,14 @@ class JugState {
         return result;
     }
 
-    // @Override
-    // public int hashCode() {
-    // int hash = 3;
-    // hash = 53 * hash
-    // + ((this.jug1 != null && this.jug2 != null) ? (this.jug1.hashCode() +
-    // this.jug1.hashCode()) : 0);
-    // hash = 53 * hash + this.jug1 + this.jug2;
-    // return hash;
-    // }
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash
+                + ((this.jug1 != null && this.jug2 != null) ? (this.jug1.hashCode() + this.jug1.hashCode()) : 0);
+        hash = 53 * hash + this.jug1 + this.jug2;
+        return hash;
+    }
 
     @Override
     public boolean equals(Object o) {
